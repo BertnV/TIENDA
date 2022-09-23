db105ac458ef9e7806cb8f10e40893c415179609
BertnV <bertin-9@hotmail.com>
2022-09-23 17:26:50 -0500

    Login

--- action/login.php
+++ action/login.php
@@ -1,8 +1,36 @@
 <?php
 
-print_r($_POST);
+    //print_r($_POST);
 
+    include('../connection/connection.php');
 
+    $phpEmail = $_POST['inputEmail'];
+    $phpPassword = $_POST['inputPassword'];
 
+    $consulta = "SELECT * FROM usuario WHERE email = '$phpEmail'";
+    // $consulta2 = "SELECT * FROM usuario WHERE passwor = '$phpPassword'";
+ 
+    $resultado = mysqli_query($connection,$consulta);
 
+    $fila = mysqli_fetch_array($resultado);
+
+    $respuesta = ''; // Comprobación
+
+    if($fila > 0 &&  $fila["password"] = $phpPassword){
+        if($fila["email"] = $phpEmail ){
+            $respuesta = 1;
+        }else{
+            $respuesta = "El password es incorrecto";
+            echo $respuesta;
+            echo '<META HTTP-EQUIV="REFRESH" CONTENT="5;URL=../index.html">';
+        }
+    }else{
+        $respuesta = "El correo electronico no es correcto";
+        echo $respuesta;
+        echo '<META HTTP-EQUIV="REFRESH" CONTENT="5;URL=../index.html">';
+    }
+
+    if($respuesta == 1){
+        header("location: ../dashboard.html");
+    }
 ?>
